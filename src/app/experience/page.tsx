4af0a6607b90259a8c35096bb7931f69157cc6f9
ZL&EL <lzc123456ab@gmail.com>
2024-04-15 00:04:40 -0400

    1

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,7 @@
+export default function experience() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>this is my experience</p>
+      </main>
+    );
+  }
---
+++ src/app/notes/page.tsx
@@ -0,0 +1,7 @@
+export default function Notes() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>notes</p>
+        </main>
+    );
+    }
