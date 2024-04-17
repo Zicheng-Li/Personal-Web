a9c794eee2dcf3f975da7bd617594ee1ade72af8
ZL&EL <lzc123456ab@gmail.com>
2024-04-17 00:05:44 -0400

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,37 @@
+'use client'
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar shouldHideOnScroll isBordered>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Experience
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Projects
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Notes
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contact
+          </Link>
+        </NavbarItem>
+
+
+      </NavbarContent>
+      </Navbar>
       <p>welcome to my website</p>
     </main>
   );
