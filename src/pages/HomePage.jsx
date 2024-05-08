c8247b6ff887cb3cedbd9533d5ceae1f53f774d6
ZL&EL <lzc123456ab@gmail.com>
2024-05-07 23:27:10 -0400

    1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,14 @@ import '../App.css';
 
 const HomePage = () => {
   return (
-    <div className='raleway-dots-regular' ><h1 class="text-red-500 hover:text-blue-600">HomePage</h1></div>
+    <div className='raleway-dots-regular' >
+      
+      <h2 class="text-red-500 hover:text-blue-600">HomePage</h2>
+
+    
+    
+    
+    </div>
   )
 }
 
