281fc103d870da7fab13acf92e1e99cfe06cc7a5
ZL&EL <lzc123456ab@gmail.com>
2024-05-08 22:22:15 -0400

    1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
-    <div >
-        <nav style={{maxWidth: '34rem', marginTop: '2rem'}} className=" flex justify-center items-center bg-opacity-90 backdrop-filter backdrop-blur-lg bg-gray-800 text-white shadow-lg rounded-full py-1 px-2  mx-auto ">
+    // backdrop-hue-rotate-30 backdrop-brightness-200 
+    <div >    
+        <nav style={{maxWidth: '34rem', marginTop: '2rem'}} className=" flex justify-center items-center bg-transparent backdrop-filter backdrop-opacity-40   backdrop-blur-sm text-white shadow-lg rounded-full py-1 px-2 mx-auto ">
           <ul className="flex list-none p-0 space-x-4">
             <li className="px-4 py-2 hover:bg-gray-700 rounded-full transition duration-300 ease-in-out transform hover:scale-105 active:scale-95">
               <Link to="/">Me</Link>
