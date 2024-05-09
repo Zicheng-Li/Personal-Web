ac717c5c0686eebdd98cfc87dc53084e586b581e
ZL&EL <lzc123456ab@gmail.com>
2024-05-08 22:05:26 -0400

    1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,16 +3,12 @@ import '../App.css';
 
 const HomePage = () => {
   return (
-    <div className='raleway-dots-regular' >
-      
-      <h5 class="text-red-500 hover:text-blue-600">HomePage</h5>
-      <p class="text-red-500 hover:text-blue-600">now for some</p>
-
-    
-    
-    
+    <div className="flex flex-col items-center">
+      <h1 style={{ fontSize: "clamp(3.2rem, 0.4rem + 5vw, 13.75rem)" }} className="text-white hover:text-blue-500 transition-colors duration-600 ease-in-out   merienda-try ">Hi. I'm Evan.</h1>
+      <h1 style={{ fontSize: "clamp(3.2rem, 0.4rem + 5vw, 13.75rem)" }} className="text-white hover:text-cyan-500 transition-colors duration-600 ease-in-out   merienda-try  ">A Developer</h1>
+      <p className="merienda-try text-orange-500 hover:text-emerald-600 transition-colors duration-600 ease-in-out">A passionate Full-Stack Developer and AI Enthusiast.</p>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
