1fd57df7eca741287f103cb917ffb2dbdb5a30f6
ZL&EL <lzc123456ab@gmail.com>
2024-05-16 22:39:48 -0400

    1

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -21,7 +21,7 @@ const ContactPage = () => {
               radius="full"
               size="sm"
               variant="solid"
-              css={{ backgroundColor: '#0077B5', borderColor: '#0077B5' }}
+              style={{ backgroundColor: '#0077B5', borderColor: '#0077B5' }}
             >
               Follow me on <i className="fa-brands fa-linkedin"></i>
             </Button>
@@ -45,23 +45,23 @@ const ContactPage = () => {
       </Card>
 
 
-      <Card className="mt-16 max-w-[340px] bg-transparent">
+      <Card className="mt-16 min-w-[340px] max-w-[340px] bg-transparent">
         <CardHeader className="justify-between">
           <div className="flex gap-5 items-center">
             <Avatar isBordered radius="full" size="xl" src={myImg} />
             <div className="flex flex-col gap-1 items-start justify-center">
               <h4 className="text-xl font-semibold leading-none text-white">Evan Li</h4>
-              <h5 className="text-xs tracking-tight font-light opacity-85 text-gray-300">@Zicheng-Li </h5>
+              <h5 className="text-xs tracking-tight font-light opacity-85 text-gray-300">@Zicheng-Li</h5>
             </div>
           </div>
           <a href="https://github.com/Zicheng-Li" target="_blank" rel="noopener noreferrer">
             <Button
-              className="flex items-center gap-1 mt-2"
+              className="flex items-center gap-1 mt-2 github-button"
               color="primary"
               radius="full"
               size="sm"
               variant="solid"
-              css={{ backgroundColor: '#0077B5', borderColor: '#0077B5' }}
+              style={{ backgroundColor: '#000', borderColor: '#000', color: '#fff' }} // 强制覆盖颜色
             >
               Follow me on <i className="fa-brands fa-github"></i>
             </Button>
@@ -69,18 +69,22 @@ const ContactPage = () => {
         </CardHeader>
         <CardBody className="px-3 py-0 text-small text-default-400">
           <p className="text-white">
-            Aspiring Full-Stack Developer and AI Enthusiast 🚀
+            Focusing 🎯
           </p>
-          <span className="pt-2 text-white">
-            Computer Science Student 💻
-          </span>
         </CardBody>
         <CardFooter className="gap-3">
-          <div className="flex gap-1">
-            <p className="font-semibold text-white text-small">121</p>
-            <p className="text-white text-small">Connections</p>
+          <div className="flex gap-1 flex-col">
+            <div className='flex items-center'>
+            <p className="font-semibold text-white text-small">700  </p>
+            <p className="text-white text-small ml-[5px]">contributions in the last year 🛠️</p>
+            </div>
+            <div className='flex items-center'>
+
+              <p className="font-semibold text-white text-small">38</p>
+            <p className="text-white text-small ml-[5px]">repositories 📁</p>
+            </div>
+            
           </div>
-          
         </CardFooter>
       </Card>
     </div>
