78ca96021c222f0fd19bdaeb04740f8f09551c4a
ZL&EL <lzc123456ab@gmail.com>
2024-05-16 21:38:26 -0400

    1

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card, CardHeader, CardBody, CardFooter, Avatar, Button} from "@nextui-org/react";
+import myImg from '../assets/me.JPG';
 const ContactPage = () => {
   return (
     <div  className="flex flex-col items-center py-10 mt-8">
@@ -7,25 +8,24 @@ const ContactPage = () => {
       <Card className="max-w-[340px] bg-transparent">
       <CardHeader className="justify-between">
         <div className="flex gap-5">
-          <Avatar isBordered radius="full" size="md" src="https://nextui.org/avatars/avatar-1.png" />
+          <Avatar isBordered radius="full" size="md" src={myImg} />
           <div className="flex flex-col gap-1 items-start justify-center">
             <h4 className="text-small font-semibold leading-none text-default-600" style={{ color: 'white' }}>Evan Li</h4>
-            <h5 className="text-small tracking-tight text-default-400" style={{ color: 'white' }}>@Canada</h5>
+            <h5 className="text-xs tracking-tight font-light opacity-85" style={{ color: 'white' }}>@OPG</h5>
           </div>
         </div>
         <a href="https://www.linkedin.com/in/evan-li-873078256/" target="_blank" rel="noopener noreferrer">
         <Button
-          className={""}
+          className="flex items-center gap-1"
           color="primary"
           radius="full"
           size="sm"
           variant={"solid"}
-          
         >
-          Follow me on LinkedIn
+          Follow me on <i className="fa-brands fa-linkedin"></i>
         </Button>
+      </a>
 
-        </a>
       </CardHeader>
       <CardBody className="px-3 py-0 text-small text-default-400">
         <p>
@@ -33,20 +33,13 @@ const ContactPage = () => {
         </p>
         <span className="pt-2">
           #Computer Science Student 💻
-          {/* <span className="py-2" aria-label="computer" role="img">
-             💻
-          </span> */}
         </span>
       </CardBody>
       <CardFooter className="gap-3">
         <div className="flex gap-1">
-          <p className="font-semibold text-default-400 text-small">120</p>
+          <p className="font-semibold text-default-400 text-small">121</p>
           <p className=" text-default-400 text-small">Connections</p>
         </div>
-        {/* <div className="flex gap-1">
-          <p className="font-semibold text-default-400 text-small">97.1K</p>
-          <p className="text-default-400 text-small">Followers</p>
-        </div> */}
       </CardFooter>
     </Card>
 
