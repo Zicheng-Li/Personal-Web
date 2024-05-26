17d8cc4486dcf3e1f03b9dbb22ce759652c514e8
ZL&EL <lzc123456ab@gmail.com>
2024-05-26 19:59:41 -0400

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
     <footer className="bg-transparent text-white py-4 mt-10">
   <div className="container mx-auto flex flex-row justify-evenly items-start px-4">
   <div className="flex flex-col space-y-4 items-start">
-    <div style={{color: "rgb(7,142,83)"}} className=' font-medium text-base'>Go To</div>
+    <div  className='text-teal-400 font-medium text-base'>Go To</div>
     <button onClick={() => handleNavigate('/')} className='text-sm text-slate-200 hover:text-teal-500 transition duration-300 ease-in-out'>Me</button>
     <button onClick={() => handleNavigate('/experiences')} className='text-sm text-slate-200 hover:text-teal-500 transition duration-300 ease-in-out'>Experiences</button>
     <button onClick={() => handleNavigate('/projects')} className='text-sm text-slate-200 hover:text-teal-500 transition duration-300 ease-in-out'>Projects</button>
@@ -21,14 +21,14 @@ function Footer() {
     </div>
     
     <div className="flex flex-col space-y-4 items-start">
-    <div style={{color: "rgb(7,142,83)"}} className=' font-medium text-base'>Elsewhere</div>
+    <div  className='text-teal-400 font-medium text-base'>Elsewhere</div>
       <a href="https://github.com/Zicheng-Li" target="_blank" rel="noopener noreferrer" className="text-slate-200 hover:text-teal-500 transition duration-300 ease-in-out mr-4 text-sm">Github</a>
       <a href="https://www.linkedin.com/in/evan-li-873078256/" target="_blank" rel="noopener noreferrer" className="text-slate-200 hover:text-teal-500 transition duration-300 ease-in-out text-sm">LinkedIn</a>
       <a href="https://docs.google.com/gview?url=https://github.com/Zicheng-Li/resume/raw/master/resume.pdf&embedded=true" target="_blank" rel="noopener noreferrer" className="text-slate-200 hover:text-teal-500 transition duration-300 ease-in-out mr-4 text-sm">Resume</a>
 
     </div>
     <div className="flex flex-col space-y-4 items-start">
-        <div style={{color: "rgb(7,142,83)"}} className=' font-medium text-base '>Contact</div>
+        <div className='text-teal-400 font-medium text-base '>Contact</div>
       <a href="mailto:li1276@mcmaster.ca" className="text-slate-200 text-sm hover:text-teal-500 transition duration-300 ease-in-out ">Email</a>
     </div>
   </div>
--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -16,18 +16,18 @@ const ExperiencePage = () => {
             className="vertical-timeline-element--education"
             contentStyle={{ background: 'rgb(2 132 199)', color: '#fff' }}
             contentArrowStyle={{ borderRight: '7px solid rgba(255, 255, 255, 0.5)' }}
-            date="2021 - present"
+            date="September 2021 - present"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', width: '50px', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'absolute', left: 'calc(50% + 5px)', top: '0' }}
             icon={<i className="fas fa-school" style={{ fontSize: '25px' }} />}
           >
             <img src={myMaster} alt="Description" style={{ width: '100%', marginTop: '10px' }} />
-            <h3 className="vertical-timeline-element-title">Bachelor of Applied Science (BASc), Computer Science, Co-op</h3>
-            <h4 className="vertical-timeline-element-subtitle">Sep 2021 - Apr 2026</h4>
-            <p>GPA of 3.8 </p>
+            <h3  className="vertical-timeline-element-title text-xl">Bachelor of Applied Science, Computer Science, Co-op</h3>
+            <h4 className="vertical-timeline-element-subtitle opacity-80">Hamilton, Canada</h4>
+            <p className='opacity-85' style={{ color: 'white',fontSize: '16px',lineHeight: '1.25rem' }}>Computer science courses include: data structures and algorithms, design patterns, databases, cryptography, and computer architecture, with a maintained GPA of 3.8. </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
         <div className='flex justify-center'>
-          <i className="fa-solid fa-angles-down mt-20 animate-bounce text-white"></i>
+          <i className="fa-solid fa-angles-down mt-10 animate-bounce text-white"></i>
           </div>
     
 
@@ -39,15 +39,15 @@ const ExperiencePage = () => {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'rgb(2 132 199)', color: '#fff' }}
             contentArrowStyle={{ borderRight: '7px solid rgba(255, 255, 255, 0.5)' }}
-            date="Jan 2024 - present"
+            date="January 2024 - present"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', width: '50px', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'absolute', left: 'calc(50% + 5px)', top: '0' }}
             icon={<i className="fas fa-briefcase" style={{ fontSize: '25px' }} />}
           >
             <img src={myImg} alt="Description" style={{ width: '100%', marginTop: '10px' }} />
-            <h3 className="vertical-timeline-element-title">Software Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bowmanville, Canada</h4>
-            <p>Developed a full-stack web application using Angular and .NET, revolutionizing the nuclear operation team’s dashboard, enhanced data visualization and access for power generation and crew metrics, reduc- ing retrieval times from 10 minutes to instant. Significantly improved operational efficiency and achieved cost savings of over $1M.</p>
-            <p>Automated a manual 2-hour equipment check process to just 1 minute using Python, pandas, and openpyxl for data processing, achieving a 120-fold increase in operational efficiency with a user-friendly Tkinter interface.</p>
+            <h3 className="vertical-timeline-element-title text-2xl">Software Engineer Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle opacity-85">Bowmanville, Canada</h4>
+            <p style={{ color: 'white',fontSize: '16px',lineHeight: '1.25rem' }} className='opacity-85'>In January 2024, I began an eight-month internship at Ontario Power Generation as a Software Engineer Intern. I utilized my full-stack development skills to create an operations dashboard using Angular and .NET, significantly enhancing the nuclear operation team's efficiency in monitoring power station operations. This dashboard reduced data retrieval times from 10 minutes to instant and achieved cost savings of over $1M. </p>
+            <p style={{ color: 'white',fontSize: '16px',lineHeight: '1.25rem' }} className='opacity-85'>Additionally, I automated a manual 2-hour equipment check process down to 1 minute using Python, pandas, and openpyxl, boosting operational efficiency by 120 times through a user-friendly Tkinter interface.</p>
           </VerticalTimelineElement>
           
           <VerticalTimelineElement
@@ -59,10 +59,10 @@ const ExperiencePage = () => {
             icon={<i className="fas fa-briefcase" style={{ fontSize: '25px' }} />}
           >
             <img src={mySVG} alt="Description" style={{ width: '100%', marginTop: '10px' }} />
-            <h3 className="vertical-timeline-element-title">Software Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Oakville, Canada</h4>
-            <p>Designed and implemented a multi-platform chatbot using Power Virtual Agent, seamlessly integrat- ing with Microsoft Teams and web platforms; utilized Power Automate for business logic to automate the transfer of Invoice, New Customer, and Purchase Order Entry data to D365, enhancing customer data processing efficiency.</p>
-            <p>Developed a mobile application using Power Apps, enabling users to capture images of items; inte- grated Azure’s Computer Vision API for image detection, streamlining the asset recognition process.</p>
+            <h3 className="vertical-timeline-element-title text-2xl">Software Engineer Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle opacity-85">Oakville, Canada</h4>
+            <p style={{ color: 'white',fontSize: '16px',lineHeight: '1.25rem' }} className='opacity-85'>In my initial industry experience at AXSource within the newly formed AI team, I designed and implemented a multi-platform chatbot using Power Virtual Agent, which integrates seamlessly with Microsoft Teams and web platforms. I utilized Power Automate to automate the transfer of Invoice, New Customer, and Purchase Order Entry data to Dynamics 365, significantly enhancing customer data processing efficiency.</p>
+            <p style={{ color: 'white',fontSize: '16px',lineHeight: '1.25rem' }} className='opacity-85'>Additionally, I developed a mobile application using Power Apps that integrates Azure’s Computer Vision API for image detection, streamlining the asset recognition process. My efforts in these projects led to earning two Microsoft certifications: Azure AI Engineer Associate and Dynamics 365 Fundamentals (ERP).</p>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
