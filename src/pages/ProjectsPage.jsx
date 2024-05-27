b44c665ba73cc539b403dd73321a1664c0f5be23
ZL&EL <lzc123456ab@gmail.com>
2024-05-27 00:11:00 -0400

    1

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import ELshop  from '../assets/ShopLogo.png';
 const ProjectsPage = () => {
   return (
 <div className="flex flex-col items-center py-10 mt-5">
@@ -11,23 +11,24 @@ const ProjectsPage = () => {
         <img
           alt="Card background"
           className="object-cover rounded-xl"
-          src="https://nextui.org/images/hero-card-complete.jpeg"
+          src={ELshop}
           width={270}
         />
-        <a href="https://github.com/your-repo-link" target="_blank" rel="noopener noreferrer" className="absolute top-0 right-0 mt-2 mr-2 text-white">
+        <a href="https://github.com/Zicheng-Li/Angular-ecommerce-frontend" target="_blank" rel="noopener noreferrer" className="absolute top-0 right-0 mt-2 mr-2 text-white">
           <i className="fab fa-github fa-lg bg-transparent hover:bg-sky-500 rounded-full transition duration-300 ease-in-out transform hover:scale-105 active:scale-90"></i>
         </a>
       </div>
       <div className="pb-0 pt-2 px-4 flex-col items-start text-left">
         <h4 className="font-bold text-large" style={{ color: 'white' }}>EL's Online Shop</h4>
         <small className="flex-1 text-default-500 font-light opacity-85" style={{ color: 'white' }}>
-          This repository contains the frontend code for EL's Online Shop, a dynamic e-commerce platform developed using Angular 16.
+        EL's Online Shop is a dynamic e-commerce platform I developed, utilizing Angular 16 for the front end and Spring Boot 3 for the back end. Key integrations include Okta's Auth0 for secure authentication and Stripe for payment processing. 
         </small>
         <div className="flex mt-4 space-x-2 text-sm">
-          <span className="text-blue-500">#react</span>
-          <span className="text-green-500">#react-router</span>
-          <span className="text-red-500">#chakra-ui</span>
-          <span className="text-yellow-500">#coingecko-api</span>
+          <span className="text-sky-500">#Angular</span>
+          <span className="text-emerald-500">#SpringBoot</span>
+          <span className="text-orange-500">#PrimeNG-UI</span>
+          <span className="text-yellow-500">#Auth0</span>
+          <span className="text-pink-500">#Stripe</span>
         </div>
       </div>
     </div>
@@ -37,7 +38,7 @@ const ProjectsPage = () => {
         <img
           alt="Card background"
           className="object-cover rounded-xl"
-          src="https://nextui.org/images/hero-card-complete.jpeg"
+          src={ELshop}
           width={270}
         />
         <a href="https://github.com/your-repo-link" target="_blank" rel="noopener noreferrer" className="absolute top-0 right-0 mt-2 mr-2 text-white ">
@@ -68,7 +69,7 @@ const ProjectsPage = () => {
         />
       </div>
       <div className="pb-0 pt-2 px-4 flex-col items-start text-left">
-        <h4 className="font-bold text-large" style={{ color: 'white' }}>EL's Online Shop</h4>
+        <h4 className="font-bold text-large" style={{ color: 'white' }}>Stealth Startup</h4>
         <small className="flex-1 text-default-500 font-light opacity-85" style={{ color: 'white' }}>
           This repository contains the frontend code for EL's Online Shop, a dynamic e-commerce platform developed using Angular 16.
         </small>
